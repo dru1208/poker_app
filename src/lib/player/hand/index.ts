c83ca2198c715baa18165096da5690168af6a1f3
andrew hsieh <andrewhsieh1208@gmail.com>
2020-04-16 17:17:45 -0400

    created player type with hands

---
+++ src/lib/player/hand/index.ts
@@ -0,0 +1,11 @@
+import { Card } from "../card"
+
+class Hand {
+  cards: Card[]
+
+  constructor(cards) {
+    this.cards = cards
+  }
+}
+
+export default Hand
---
+++ src/lib/player/index.ts
@@ -0,0 +1,11 @@
+import Hand from "./hand"
+
+class Player {
+  hand: Hand;
+
+  constructor(hand) {
+    this.hand = hand
+  }
+}
+
+export default Player
